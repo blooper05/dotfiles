c2793105006bdd164a56db16254a43365e659636
blooper05 <legend.of.blooper@gmail.com>
2018-08-30 21:58:21 +0900

    update

--- setup.sh
+++ setup.sh
@@ -58,6 +58,9 @@ sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
 # System Preferences >> Displays >> Night Shift >> to: 3:59
 # System Preferences >> Displays >> Night Shift >> Color Temperature
 
+# System Preferences >> Keyboard >> Keyboard >> Key Repeat
+defaults write NSGlobalDomain KeyRepeat -int 2
+
 sudo scutil --set ComputerName まっくぶっくぷろ
 sudo scutil --set LocalHostName MacBookPro
 
