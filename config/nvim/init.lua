f4926f47415e824a0f02b2bcb9c32ec6bd0ff60c
blooper05 <legend.of.blooper@gmail.com>
2021-01-22 18:12:58 +0900

    update

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -28,6 +28,23 @@ vim.api.nvim_set_keymap('n', '<F6>', ':<C-u>source $MYVIMRC<CR>', { noremap = tr
 -- Exit terminal mode easily.
 vim.api.nvim_set_keymap('t', '<ESC>', [[<C-\><C-n>]], { noremap = true, silent = true })
 
+-- Search {{{1
+
+-- Ignore the case of normal letters.
+vim.o.ignorecase = true
+
+-- If the search pattern contains upper case characters, override ignorecase option.
+vim.o.smartcase = true
+
+-- Enable incremental search.
+vim.o.incsearch = true
+
+-- Highlight search result.
+vim.o.hlsearch = true
+
+-- Clear highlight.
+vim.api.nvim_set_keymap('n', '<ESC><ESC>', ':<C-u>nohlsearch<CR>', { noremap = true, silent = true })
+
 -- Folding {{{1
 
 vim.bo.modeline = true
