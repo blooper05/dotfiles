f1234638a84b9033cc8ea6c76d65f82dd73417f7
blooper05 <legend.of.blooper@gmail.com>
2021-01-22 18:12:15 +0900

    update

---
+++ config/nvim/init.lua
@@ -0,0 +1,38 @@
+-- Basic {{{1
+
+-- Use English interface.
+vim.cmd('language messages C')
+vim.cmd('language time C')
+
+-- Settings of the encoding to use for a save and reading.
+vim.o.encoding      = 'utf-8'
+vim.o.fileencodings = 'utf-8,euc-jp,sjis,cp932,iso-2022-jp'
+
+-- Auto reload if file is changed.
+vim.bo.autoread = true
+
+-- Use system clipboard.
+vim.o.clipboard = table.concat({
+  'unnamed',
+  'unnamedplus',
+  vim.o.clipboard,
+}, ',')
+
+-- Don't redraw during macro execution.
+vim.o.lazyredraw = true
+
+-- Edit and reload vimrc immediately.
+vim.api.nvim_set_keymap('n', '<F5>', ':<C-u>tabnew $MYVIMRC<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F6>', ':<C-u>source $MYVIMRC<CR>', { noremap = true, silent = true })
+
+-- Exit terminal mode easily.
+vim.api.nvim_set_keymap('t', '<ESC>', [[<C-\><C-n>]], { noremap = true, silent = true })
+
+-- Folding {{{1
+
+vim.bo.modeline = true
+vim.o.modelines = 3
+vim.o.fillchars = [[vert:|]]
+-- vim: foldmethod=marker
+-- vim: foldcolumn=3
+-- vim: foldlevel=0
