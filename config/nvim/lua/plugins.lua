18f535a3fdb8cba6a41f8f42c6a8f91f6743ff76
blooper05 <legend.of.blooper@gmail.com>
2021-01-26 01:29:03 +0900

    update

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -176,6 +176,23 @@ return require('packer').startup(function()
   end,
   }
 
+  -- Search {{{1
+
+  use { 'kevinhwang91/nvim-hlslens', config = function()
+    vim.api.nvim_set_keymap('', 'n', [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]], { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('', 'N', [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]], { noremap = true, silent = true })
+  end,
+  }
+
+  use { 'haya14busa/vim-asterisk', config = function()
+    vim.api.nvim_set_keymap('', '*',  [[<Plug>(asterisk-z*)<Cmd>lua require('hlslens').start()<CR>]],  {})
+    vim.api.nvim_set_keymap('', '#',  [[<Plug>(asterisk-z#)<Cmd>lua require('hlslens').start()<CR>]],  {})
+    vim.api.nvim_set_keymap('', 'g*', [[<Plug>(asterisk-gz*)<Cmd>lua require('hlslens').start()<CR>]], {})
+    vim.api.nvim_set_keymap('', 'g#', [[<Plug>(asterisk-gz#)<Cmd>lua require('hlslens').start()<CR>]], {})
+  end,
+    requires = { 'kevinhwang91/nvim-hlslens' },
+  }
+
 end)
 
 -- Folding {{{1
