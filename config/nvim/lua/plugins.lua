d0767e8910c67b5dad79f14b661b8e992521a699
blooper05 <legend.of.blooper@gmail.com>
2021-11-17 13:03:09 +0900

    update

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -3,10 +3,10 @@
 local execute = vim.api.nvim_command
 local fn      = vim.fn
 
-local install_path = fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
+local installPath = fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
 
-if fn.empty(fn.glob(install_path)) > 0 then
-  execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+if fn.empty(fn.glob(installPath)) > 0 then
+  execute('!git clone https://github.com/wbthomason/packer.nvim ' .. installPath)
   execute('packadd packer.nvim')
 end
 
