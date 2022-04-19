8bd91b1f990afd13e9c8b6ce4d2fa565cd493082
blooper05 <legend.of.blooper@gmail.com>
2022-04-19 16:27:06 +0900

    update

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -100,7 +100,10 @@ return {
       })
 
       cmp.setup.cmdline('/', {
-        mapping = cmp.mapping.preset.cmdline(),
+        mapping = cmp.mapping.preset.cmdline({
+          ['<C-n>'] = cmp.config.disable,
+          ['<C-p>'] = cmp.config.disable,
+        }),
         sources = cmp.config.sources({
           { name = 'nvim_lsp_document_symbol' },
         }, {
@@ -109,7 +112,10 @@ return {
       })
 
       cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
+        mapping = cmp.mapping.preset.cmdline({
+          ['<C-n>'] = cmp.config.disable,
+          ['<C-p>'] = cmp.config.disable,
+        }),
         sources = cmp.config.sources({
           { name = 'path' },
         }, {
