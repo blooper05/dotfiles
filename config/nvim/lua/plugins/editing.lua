0b53e6d4b0ee72d6394fcfe593b256f7cf20c03b
blooper05 <legend.of.blooper@gmail.com>
2022-08-26 12:48:54 +0900

    update

--- config/nvim/lua/plugins/editing.lua
+++ config/nvim/lua/plugins/editing.lua
@@ -30,12 +30,14 @@ return {
   {
     'monaqa/dial.nvim',
     config = function()
-      vim.keymap.set('n', '<C-a>',  require('dial.map').inc_normal(),  {})
-      vim.keymap.set('n', '<C-x>',  require('dial.map').dec_normal(),  {})
-      vim.keymap.set('v', '<C-a>',  require('dial.map').inc_visual(),  {})
-      vim.keymap.set('v', '<C-x>',  require('dial.map').dec_visual(),  {})
-      vim.keymap.set('v', 'g<C-a>', require('dial.map').inc_gvisual(), {})
-      vim.keymap.set('v', 'g<C-x>', require('dial.map').dec_gvisual(), {})
+      local map = require('dial.map')
+
+      vim.keymap.set('n', '<C-a>',  map.inc_normal(),  {})
+      vim.keymap.set('n', '<C-x>',  map.dec_normal(),  {})
+      vim.keymap.set('v', '<C-a>',  map.inc_visual(),  {})
+      vim.keymap.set('v', '<C-x>',  map.dec_visual(),  {})
+      vim.keymap.set('v', 'g<C-a>', map.inc_gvisual(), {})
+      vim.keymap.set('v', 'g<C-x>', map.dec_gvisual(), {})
 
       local augend = require('dial.augend')
 
