3037c5ce648fc55e9df3687f51b6f289a2571ba8
blooper05 <legend.of.blooper@gmail.com>
2022-09-07 17:45:34 +0900

    update

--- config/nvim/lua/plugins/filer.lua
+++ config/nvim/lua/plugins/filer.lua
@@ -29,7 +29,14 @@ return {
       { 'nvim-telescope/telescope.nvim', opt = true },
     },
     config = function()
-      require('project_nvim').setup({})
+      require('project_nvim').setup({
+        sync_root_with_cwd  = true,
+        respect_buf_cwd     = true,
+        update_focused_file = {
+          enable      = true,
+          update_root = true,
+        },
+      })
 
       require('telescope').load_extension('projects')
     end,
