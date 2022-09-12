5ac904d69cce83ae11d737b2376604eb06327830
blooper05 <legend.of.blooper@gmail.com>
2022-09-13 01:07:07 +0900

    update

--- config/nvim/lua/plugins/search.lua
+++ config/nvim/lua/plugins/search.lua
@@ -15,13 +15,14 @@ return {
     requires = {
       { 'kevinhwang91/nvim-hlslens', opt = true },
     },
+    setup = function()
+      vim.g['asterisk#keeppos'] = true
+    end,
     config = function()
       vim.keymap.set('', '*',  [[<Plug>(asterisk-z*)<Cmd>lua require('hlslens').start()<CR>]],  { remap = true })
       vim.keymap.set('', '#',  [[<Plug>(asterisk-z#)<Cmd>lua require('hlslens').start()<CR>]],  { remap = true })
       vim.keymap.set('', 'g*', [[<Plug>(asterisk-gz*)<Cmd>lua require('hlslens').start()<CR>]], { remap = true })
       vim.keymap.set('', 'g#', [[<Plug>(asterisk-gz#)<Cmd>lua require('hlslens').start()<CR>]], { remap = true })
-
-      vim.g['asterisk#keeppos'] = true
     end,
     event = 'VimEnter',
   },
