3f3147f569f4c156ec398fa8dc01fe70d2b937d0
blooper05 <legend.of.blooper@gmail.com>
2022-09-13 01:06:33 +0900

    update

--- config/nvim/lua/plugins/search.lua
+++ config/nvim/lua/plugins/search.lua
@@ -7,7 +7,7 @@ return {
       vim.keymap.set('n', 'n', function() vim.cmd([[execute('normal! ' . v:count1 . 'n')]]) hlslens.start() end, { silent = true })
       vim.keymap.set('n', 'N', function() vim.cmd([[execute('normal! ' . v:count1 . 'N')]]) hlslens.start() end, { silent = true })
     end,
-    event = 'VimEnter',
+    keys = { 'n', 'N' },
   },
 
   {
