03b87d5a4433658e47038a50fde20823ccb7cfc5
blooper05 <legend.of.blooper@gmail.com>
2022-09-13 12:22:17 +0900

    update

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -4,6 +4,6 @@ return {
   font = wezterm.font('HackGen35 Console NFJ'),
   font_size = 15.0,
   use_ime = true,
-  color_scheme = 'nord',
+  color_scheme = 'nordfox',
   adjust_window_size_when_changing_font_size = false,
 }
