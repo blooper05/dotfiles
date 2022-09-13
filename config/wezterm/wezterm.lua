0796381d1fc7430b0f05a51596a1344f95a8aab5
blooper05 <legend.of.blooper@gmail.com>
2022-09-13 12:39:25 +0900

    update

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,7 +3,5 @@ local wezterm = require('wezterm')
 return {
   font = wezterm.font('HackGen35 Console NFJ'),
   font_size = 15.0,
-  use_ime = true,
   color_scheme = 'nordfox',
-  adjust_window_size_when_changing_font_size = false,
 }
