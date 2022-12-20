ffd26a8c98bad41446345d34f1abd95bf8e9cdd6
blooper05 <legend.of.blooper@gmail.com>
2022-12-20 17:04:00 +0900

    update

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -218,39 +218,37 @@ return {
     },
     config = function()
       local autopairs = require('nvim-autopairs')
-      local rule      = require('nvim-autopairs.rule')
 
       autopairs.setup({
         check_ts = true,
       })
 
+      local rule = require('nvim-autopairs.rule')
+      local brackets = { { '(', ')' }, { '[', ']' }, { '{', '}' } }
+
       autopairs.add_rules({
         rule(' ', ' '):with_pair(function(opts)
           local pair = opts.line:sub(opts.col - 1, opts.col)
-          return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+          return vim.tbl_contains({
+            brackets[1][1] .. brackets[1][2],
+            brackets[2][1] .. brackets[2][2],
+            brackets[3][1] .. brackets[3][2],
+          }, pair)
         end),
-
-        rule('( ', ' )')
-          :with_pair(function() return false end)
-          :with_move(function(opts)
-            return opts.prev_char:match('.%)') ~= nil
-          end)
-          :use_key(')'),
-
-        rule('{ ', ' }')
-          :with_pair(function() return false end)
-          :with_move(function(opts)
-            return opts.prev_char:match('.%}') ~= nil
-          end)
-          :use_key('}'),
-
-        rule('[ ', ' ]')
-          :with_pair(function() return false end)
-          :with_move(function(opts)
-            return opts.prev_char:match('.%]') ~= nil
-          end)
-          :use_key(']'),
       })
+
+      for _, bracket in pairs(brackets) do
+        autopairs.add_rules({
+          rule(bracket[1] .. ' ', ' ' .. bracket[2])
+              :with_pair(function()
+                return false
+              end)
+              :with_move(function(opts)
+                return opts.prev_char:match('.%' .. bracket[2]) ~= nil
+              end)
+              :use_key(bracket[2]),
+        })
+      end
     end,
     event = 'InsertEnter',
   },
