0fbc77d43bd2cc4930300f177d1c077c0e639d25
blooper05 <legend.of.blooper@gmail.com>
2022-12-21 18:20:50 +0900

    update

--- config/nvim/lua/plugins/dap.lua
+++ config/nvim/lua/plugins/dap.lua
@@ -48,6 +48,28 @@ return {
   },
 
   {
+    'rcarriga/cmp-dap',
+    requires = {
+      { 'hrsh7th/nvim-cmp' },
+      { 'mfussenegger/nvim-dap' },
+    },
+    config = function()
+      require('cmp').setup({
+        enabled = function()
+          return vim.api.nvim_buf_get_option(0, 'buftype') ~= 'prompt' or require('cmp_dap').is_dap_buffer()
+        end,
+      })
+
+      require('cmp').setup.filetype({ 'dap-repl', 'dapui_watches', 'dapui_hover' }, {
+        sources = {
+          { name = 'dap' },
+        },
+      })
+    end,
+    after = { 'nvim-cmp', 'nvim-dap' },
+  },
+
+  {
     'suketa/nvim-dap-ruby',
     requires = {
       { 'mfussenegger/nvim-dap' },
