11216599cf35b529c904b937971cfbc32295eba0
blooper05 <legend.of.blooper@gmail.com>
2022-12-22 02:19:27 +0900

    update

--- config/nvim/lua/plugins/fuzzy_finder.lua
+++ config/nvim/lua/plugins/fuzzy_finder.lua
@@ -68,6 +68,26 @@ return {
   },
 
   {
+    'nvim-telescope/telescope-fzy-native.nvim',
+    requires = {
+      { 'nvim-telescope/telescope.nvim' },
+    },
+    config = function()
+      require('telescope').setup({
+        extensions = {
+          fzy_native = {
+            override_generic_sorter = false,
+            override_file_sorter = true,
+          },
+        },
+      })
+
+      require('telescope').load_extension('fzy_native')
+    end,
+    after = 'telescope.nvim',
+  },
+
+  {
     'nvim-telescope/telescope-frecency.nvim',
     requires = {
       { 'kkharji/sqlite.lua' },
