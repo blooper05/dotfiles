b9a3cd6bd8d5385c03fa5e97c9367051b84eb38b
blooper05 <legend.of.blooper@gmail.com>
2022-12-21 19:08:26 +0900

    update

--- config/nvim/lua/plugins/editing.lua
+++ config/nvim/lua/plugins/editing.lua
@@ -56,10 +56,10 @@ return {
     config = function()
       local map = require('dial.map')
 
-      vim.keymap.set('n', '<C-a>',  map.inc_normal(),  {})
-      vim.keymap.set('n', '<C-x>',  map.dec_normal(),  {})
-      vim.keymap.set('v', '<C-a>',  map.inc_visual(),  {})
-      vim.keymap.set('v', '<C-x>',  map.dec_visual(),  {})
+      vim.keymap.set('n', '<C-a>', map.inc_normal(), {})
+      vim.keymap.set('n', '<C-x>', map.dec_normal(), {})
+      vim.keymap.set('v', '<C-a>', map.inc_visual(), {})
+      vim.keymap.set('v', '<C-x>', map.dec_visual(), {})
       vim.keymap.set('v', 'g<C-a>', map.inc_gvisual(), {})
       vim.keymap.set('v', 'g<C-x>', map.dec_gvisual(), {})
 
@@ -77,9 +77,21 @@ return {
           augend.constant.alias.bool,
           augend.semver.alias.semver,
           augend.constant.new({
-            elements = { 'pick', 'fixup', 'reword', 'edit', 'squash', 'exec', 'break', 'drop', 'label', 'reset', 'merge' },
-            word     = true,
-            cyclic   = true,
+            elements = {
+              'pick',
+              'fixup',
+              'reword',
+              'edit',
+              'squash',
+              'exec',
+              'break',
+              'drop',
+              'label',
+              'reset',
+              'merge',
+            },
+            word = true,
+            cyclic = true,
           }),
         },
       })
