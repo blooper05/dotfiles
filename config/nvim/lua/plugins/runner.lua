2c2382f2ca5c4f4503fd1d11ad11a0c3fad5371c
blooper05 <legend.of.blooper@gmail.com>
2022-12-22 15:57:45 +0900

    update

--- config/nvim/lua/plugins/runner.lua
+++ config/nvim/lua/plugins/runner.lua
@@ -2,7 +2,6 @@ return {
   {
     'jose-elias-alvarez/null-ls.nvim',
     requires = {
-      { 'neovim/nvim-lspconfig' },
       { 'nvim-lua/plenary.nvim' },
     },
     config = function()
@@ -36,18 +35,17 @@ return {
           if client.supports_method('textDocument/formatting') then
             vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
             vim.api.nvim_create_autocmd('BufWritePre', {
-              group    = augroup,
-              buffer   = bufnr,
+              group = augroup,
+              buffer = bufnr,
               callback = function()
                 vim.lsp.buf.format({ bufnr = bufnr })
-              end
+              end,
             })
           end
         end,
       })
     end,
-    after = 'nvim-lspconfig',
-    event = 'BufWinEnter',
+    event = 'BufReadPre',
   },
 
   {
