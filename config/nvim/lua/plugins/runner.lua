71cd6a805cb5fcc2a7c2ca69629b5638c03bd60a
blooper05 <legend.of.blooper@gmail.com>
2022-12-22 16:49:14 +0900

    update

--- config/nvim/lua/plugins/runner.lua
+++ config/nvim/lua/plugins/runner.lua
@@ -85,7 +85,11 @@ return {
 
       neotest.setup({
         adapters = {
-          require('neotest-rspec'),
+          require('neotest-rspec')({
+            rspec_cmd = function()
+              return vim.tbl_flatten({ 'docker-rspec' })
+            end,
+          }),
         },
       })
     end,
