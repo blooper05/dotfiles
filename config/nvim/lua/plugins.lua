c6feffa58713d0f7af934cbc9dcd775412a68aaf
blooper05 <legend.of.blooper@gmail.com>
2022-12-22 19:19:02 +0900

    update

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,7 +1,7 @@
 local fn = vim.fn
 
 local installPath = fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
-local repository  = 'https://github.com/wbthomason/packer.nvim'
+local repository = 'https://github.com/wbthomason/packer.nvim'
 
 if fn.empty(fn.glob(installPath)) > 0 then
   PackerBootstrap = fn.system({ 'git', 'clone', '--depth', '1', repository, installPath })
@@ -10,38 +10,70 @@ end
 vim.cmd('packadd packer.nvim')
 
 local function packerStartup(use)
-  use { 'wbthomason/packer.nvim', opt = true,
+  use({
+    'wbthomason/packer.nvim',
+    opt = true,
     setup = function()
       -- Run :PackerCompile whenever plugins.lua is updated automatically.
       local autoPackerCompile = vim.api.nvim_create_augroup('AutoPackerCompile', { clear = true })
       vim.api.nvim_create_autocmd('BufWritePost', {
-        group   = autoPackerCompile,
+        group = autoPackerCompile,
         pattern = 'plugins.lua',
         command = 'source <afile> | PackerCompile',
       })
     end,
-  }
+  })
 
-  for _, plugin in ipairs(require('plugins.appearance'))   do use(plugin) end
-  for _, plugin in ipairs(require('plugins.treesitter'))   do use(plugin) end
+  for _, plugin in ipairs(require('plugins.appearance')) do
+    use(plugin)
+  end
+  for _, plugin in ipairs(require('plugins.treesitter')) do
+    use(plugin)
+  end
 
-  for _, plugin in ipairs(require('plugins.lsp'))          do use(plugin) end
-  for _, plugin in ipairs(require('plugins.completion'))   do use(plugin) end
+  for _, plugin in ipairs(require('plugins.lsp')) do
+    use(plugin)
+  end
+  for _, plugin in ipairs(require('plugins.completion')) do
+    use(plugin)
+  end
 
-  for _, plugin in ipairs(require('plugins.fuzzy_finder')) do use(plugin) end
-  for _, plugin in ipairs(require('plugins.filer'))        do use(plugin) end
-  for _, plugin in ipairs(require('plugins.terminal'))     do use(plugin) end
+  for _, plugin in ipairs(require('plugins.fuzzy_finder')) do
+    use(plugin)
+  end
+  for _, plugin in ipairs(require('plugins.filer')) do
+    use(plugin)
+  end
+  for _, plugin in ipairs(require('plugins.terminal')) do
+    use(plugin)
+  end
 
-  for _, plugin in ipairs(require('plugins.text_object'))  do use(plugin) end
-  for _, plugin in ipairs(require('plugins.editing'))      do use(plugin) end
-  for _, plugin in ipairs(require('plugins.search'))       do use(plugin) end
+  for _, plugin in ipairs(require('plugins.text_object')) do
+    use(plugin)
+  end
+  for _, plugin in ipairs(require('plugins.editing')) do
+    use(plugin)
+  end
+  for _, plugin in ipairs(require('plugins.search')) do
+    use(plugin)
+  end
 
-  for _, plugin in ipairs(require('plugins.dap'))          do use(plugin) end
-  for _, plugin in ipairs(require('plugins.runner'))       do use(plugin) end
+  for _, plugin in ipairs(require('plugins.dap')) do
+    use(plugin)
+  end
+  for _, plugin in ipairs(require('plugins.runner')) do
+    use(plugin)
+  end
 
-  for _, plugin in ipairs(require('plugins.filetype'))     do use(plugin) end
-  for _, plugin in ipairs(require('plugins.vcs'))          do use(plugin) end
-  for _, plugin in ipairs(require('plugins.web'))          do use(plugin) end
+  for _, plugin in ipairs(require('plugins.filetype')) do
+    use(plugin)
+  end
+  for _, plugin in ipairs(require('plugins.vcs')) do
+    use(plugin)
+  end
+  for _, plugin in ipairs(require('plugins.web')) do
+    use(plugin)
+  end
 
   if PackerBootstrap then
     require('packer').sync()
