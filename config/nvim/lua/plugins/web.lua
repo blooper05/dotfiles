4bc890716c25696b1dbf1739babc635d1d9fac16
blooper05 <legend.of.blooper@gmail.com>
2022-12-22 19:12:05 +0900

    update

--- config/nvim/lua/plugins/web.lua
+++ config/nvim/lua/plugins/web.lua
@@ -2,17 +2,17 @@ return {
   {
     'pwntester/octo.nvim',
     requires = {
-      { 'nvim-lua/plenary.nvim'                   },
-      { 'nvim-telescope/telescope.nvim'           },
+      { 'nvim-lua/plenary.nvim' },
+      { 'nvim-telescope/telescope.nvim' },
       { 'nvim-tree/nvim-web-devicons', opt = true },
     },
     setup = function()
-      vim.keymap.set('n', '[octo]',       '<Nop>',  {})
+      vim.keymap.set('n', '[octo]', '<Nop>', {})
       vim.keymap.set('n', '[telescope]G', '[octo]', { remap = true })
     end,
     config = function()
       vim.keymap.set('n', '[octo]i', function() vim.cmd('Octo issue list') end, { silent = true })
-      vim.keymap.set('n', '[octo]p', function() vim.cmd('Octo pr list')    end, { silent = true })
+      vim.keymap.set('n', '[octo]p', function() vim.cmd('Octo pr list') end, { silent = true })
 
       require('octo').setup({})
     end,
