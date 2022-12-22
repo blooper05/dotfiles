79fb1d0bf4f4bab40b53bb6068384837f524310c
blooper05 <legend.of.blooper@gmail.com>
2022-12-22 18:30:33 +0900

    update

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,22 @@
 return {
   {
-    'nvim-treesitter/nvim-treesitter', run = ':TSUpdate',
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
     config = function()
       require('nvim-treesitter.configs').setup({
-        ensure_installed      = 'all',
-        highlight             = { enable = true  },
-        incremental_selection = { enable = true  },
-        indent                = { enable = false },
+        ensure_installed = 'all',
+        highlight = {
+          enable = true,
+        },
+        incremental_selection = {
+          enable = true,
+        },
+        indent = {
+          enable = false,
+        },
       })
     end,
-    event = 'BufWinEnter',
+    event = 'BufReadPost',
   },
 
   {
