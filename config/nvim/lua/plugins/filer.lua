9377473eb604c56caaaaf5883dd70a1858d971c7
blooper05 <legend.of.blooper@gmail.com>
2023-01-11 12:55:35 +0900

    update

--- config/nvim/lua/plugins/filer.lua
+++ config/nvim/lua/plugins/filer.lua
@@ -25,8 +25,11 @@ return {
         view = {
           adaptive_size = true,
         },
+        sync_root_with_cwd = true,
+        respect_buf_cwd = true,
         update_focused_file = {
           enable = true,
+          update_root = false,
         },
       })
     end,
