601851527407aa2f0646c2722ea4ee004d257838
blooper05 <legend.of.blooper@gmail.com>
2023-02-21 12:26:16 +0900

    update

--- config/nvim/lua/plugins/search.lua
+++ config/nvim/lua/plugins/search.lua
@@ -5,16 +5,28 @@ return {
       {
         'n',
         function()
-          vim.cmd([[execute('normal! ' . v:count1 . 'n')]])
-          require('hlslens').start()
+          vim.cmd([[
+            try
+              execute('normal! ' . v:count1 . 'n')
+              lua require('hlslens').start()
+            catch /^Vim(normal):E486: Pattern not found: /
+              echo 'Pattern not found'
+            endtry
+          ]])
         end,
         silent = true,
       },
       {
         'N',
         function()
-          vim.cmd([[execute('normal! ' . v:count1 . 'N')]])
-          require('hlslens').start()
+          vim.cmd([[
+            try
+              execute('normal! ' . v:count1 . 'N')
+              lua require('hlslens').start()
+            catch /^Vim(normal):E486: Pattern not found: /
+              echo 'Pattern not found'
+            endtry
+          ]])
         end,
         silent = true,
       },
@@ -31,14 +43,10 @@ return {
       vim.g['asterisk#keeppos'] = true
     end,
     keys = {
-      { '*', [[<Plug>(asterisk-z*)<Cmd>lua require('hlslens').start()<CR>]], mode = 'n', remap = true },
-      { '#', [[<Plug>(asterisk-z#)<Cmd>lua require('hlslens').start()<CR>]], mode = 'n', remap = true },
-      { 'g*', [[<Plug>(asterisk-gz*)<Cmd>lua require('hlslens').start()<CR>]], mode = 'n', remap = true },
-      { 'g#', [[<Plug>(asterisk-gz#)<Cmd>lua require('hlslens').start()<CR>]], mode = 'n', remap = true },
-      { '*', [[<Plug>(asterisk-z*)<Cmd>lua require('hlslens').start()<CR>]], mode = 'x', remap = true },
-      { '#', [[<Plug>(asterisk-z#)<Cmd>lua require('hlslens').start()<CR>]], mode = 'x', remap = true },
-      { 'g*', [[<Plug>(asterisk-gz*)<Cmd>lua require('hlslens').start()<CR>]], mode = 'x', remap = true },
-      { 'g#', [[<Plug>(asterisk-gz#)<Cmd>lua require('hlslens').start()<CR>]], mode = 'x', remap = true },
+      { '*', [[<Plug>(asterisk-z*)<Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'x' }, remap = true },
+      { '#', [[<Plug>(asterisk-z#)<Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'x' }, remap = true },
+      { 'g*', [[<Plug>(asterisk-gz*)<Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'x' }, remap = true },
+      { 'g#', [[<Plug>(asterisk-gz#)<Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'x' }, remap = true },
     },
   },
 }
