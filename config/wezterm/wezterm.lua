fd801a2431c4cb3a4f86fcec6672092ca90b9e62
blooper05 <legend.of.blooper@gmail.com>
2023-10-30 12:12:55 +0900

    update

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,6 +1,12 @@
 local wezterm = require('wezterm')
 
+local mux = wezterm.mux
 local config = wezterm.config_builder()
+
+wezterm.on('gui-startup', function(cmd)
+  local _, _, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
 
 config.font = wezterm.font('HackGen35 Console NF')
 config.font_size = 15.0
