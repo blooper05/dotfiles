99b54819a9b3c97668cbbbb101902bec2b589a6e
blooper05 <legend.of.blooper@gmail.com>
2023-10-31 19:18:09 +0900

    update

--- config/nvim/lua/plugins/editing.lua
+++ config/nvim/lua/plugins/editing.lua
@@ -88,16 +88,10 @@ return {
   },
 
   {
-    'ntpeters/vim-better-whitespace', -- non-lua plugin
-    init = function()
-      -- Strip whitespaces when I save files.
-      vim.g.strip_whitespace_on_save = true
-      vim.g.strip_whitespace_confirm = false
-
-      vim.g.better_whitespace_filetypes_blacklist = {
-        'toggleterm',
-      }
-    end,
+    'kaplanz/retrail.nvim',
+    opts = {
+      hlgroup = 'Substitute',
+    },
     event = 'BufReadPost',
   },
 
