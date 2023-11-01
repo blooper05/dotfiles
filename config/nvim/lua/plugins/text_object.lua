560ed81162375e9296c08b5cad4c515626443f28
blooper05 <legend.of.blooper@gmail.com>
2023-11-01 18:16:18 +0900

    update

--- config/nvim/lua/plugins/text_object.lua
+++ config/nvim/lua/plugins/text_object.lua
@@ -1,7 +1,14 @@
 return {
   {
-    'chaoren/vim-wordmotion', -- non-lua plugin
-    keys = { 'w', 'W', 'b', 'B', 'e', 'E', 'ge', 'gE', 'aw', 'aW', 'iw', 'iW', '<C-R><C-W>', '<C-R><C-A>' },
+    'chrisgrieser/nvim-spider',
+    keys = {
+      -- stylua: ignore start
+      { 'w',  function() require('spider').motion('w') end,  mode = { 'n', 'o', 'x' } },
+      { 'e',  function() require('spider').motion('e') end,  mode = { 'n', 'o', 'x' } },
+      { 'b',  function() require('spider').motion('b') end,  mode = { 'n', 'o', 'x' } },
+      { 'ge', function() require('spider').motion('ge') end, mode = { 'n', 'o', 'x' } },
+      -- stylua: ignore end
+    },
   },
 
   {
