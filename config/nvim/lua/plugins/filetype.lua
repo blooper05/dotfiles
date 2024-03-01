181a6a1aec7d1cef6a50c1d04be22592c31e59d7
blooper05 <legend.of.blooper@gmail.com>
2024-03-01 17:20:17 +0900

    update

--- config/nvim/lua/plugins/filetype.lua
+++ config/nvim/lua/plugins/filetype.lua
@@ -78,6 +78,15 @@ return {
     },
   },
 
+  {
+    'lukas-reineke/headlines.nvim',
+    dependencies = {
+      { 'nvim-treesitter/nvim-treesitter' },
+    },
+    config = true,
+    ft = 'markdown',
+  },
+
   -- PlantUML
 
   {
