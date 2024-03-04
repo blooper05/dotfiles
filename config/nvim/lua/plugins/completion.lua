e12140cd1ce5991241ef1aa2c87360cc4b89892b
blooper05 <legend.of.blooper@gmail.com>
2024-03-04 10:04:38 +0900

    update

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -69,6 +69,10 @@ return {
             },
           }),
         },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
         mapping = cmp.mapping.preset.insert({
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
