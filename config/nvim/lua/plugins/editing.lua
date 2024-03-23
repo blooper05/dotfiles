85caeaaf8ee7912c4337c055b84cba5c36438437
blooper05 <legend.of.blooper@gmail.com>
2024-03-23 17:50:05 +0900

    update

--- config/nvim/lua/plugins/editing.lua
+++ config/nvim/lua/plugins/editing.lua
@@ -33,7 +33,6 @@ return {
     'kevinhwang91/nvim-ufo',
     dependencies = {
       { 'kevinhwang91/promise-async' },
-      { 'nvim-treesitter/nvim-treesitter' },
     },
     init = function()
       vim.opt.foldlevel = 99
