a0776c03d49b7d3ca5be729c49335eda573c3466
blooper05 <legend.of.blooper@gmail.com>
2024-05-07 12:05:16 +0900

    update

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -8,6 +8,14 @@ wezterm.on('gui-startup', function(cmd)
   window:gui_window():maximize()
 end)
 
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
 config.font = wezterm.font('HackGen35 Console NF')
 config.font_size = 15.0
 config.color_scheme = 'nordfox'
