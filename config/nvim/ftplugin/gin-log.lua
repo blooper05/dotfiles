48a049ac352206ddecb799d902b447003acf4920
blooper05 <legend.of.blooper@gmail.com>
2024-08-06 10:48:25 +0900

    update

--- config/nvim/ftplugin/gin-log.lua
+++ config/nvim/ftplugin/gin-log.lua
@@ -1,3 +1,4 @@
 vim.opt_local.cursorline = true
 
 vim.keymap.set('n', 'p', '<Plug>(gin-action-show:vsplit)', { buffer = true })
+vim.keymap.set('n', 'if', '<Plug>(gin-action-fixup:instant-fixup)', { buffer = true })
