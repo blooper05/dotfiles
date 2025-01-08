6950dab017432289efeb6d52d58f70e134846f6b
blooper05 <legend.of.blooper@gmail.com>
2025-01-09 01:43:05 +0900

    update

--- config/nvim/lua/plugins/vcs.lua
+++ config/nvim/lua/plugins/vcs.lua
@@ -65,6 +65,28 @@ return {
   },
 
   {
+    'isakbm/gitgraph.nvim',
+    dependencies = {
+      { 'sindrets/diffview.nvim' },
+    },
+    opts = {
+      hooks = {
+        on_select_commit = function(commit)
+          vim.cmd(':DiffviewOpen ' .. commit.hash .. '^!')
+        end,
+        on_select_range_commit = function(from, to)
+          vim.cmd(':DiffviewOpen ' .. from.hash .. '~1..' .. to.hash)
+        end,
+      },
+    },
+    keys = {
+      -- stylua: ignore start
+      { '[git]g', function() require('gitgraph').draw({}, { revision_range = '--no-show-signature' }) end, silent = true },
+      -- stylua: ignore end
+    },
+  },
+
+  {
     'akinsho/git-conflict.nvim',
     config = true,
     event = 'BufReadPost',
