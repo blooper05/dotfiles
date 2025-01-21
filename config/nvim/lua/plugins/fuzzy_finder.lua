28a8dc097411b8f4a7e6cdf752f227739cb246a5
blooper05 <legend.of.blooper@gmail.com>
2025-01-21 19:30:35 +0900

    update

--- config/nvim/lua/plugins/fuzzy_finder.lua
+++ config/nvim/lua/plugins/fuzzy_finder.lua
@@ -65,6 +65,25 @@ return {
   },
 
   {
+    'nvim-telescope/telescope-ui-select.nvim',
+    dependencies = {
+      { 'nvim-telescope/telescope.nvim' },
+    },
+    config = function()
+      require('telescope').setup({
+        extensions = {
+          ['ui-select'] = {
+            require('telescope.themes').get_dropdown({}),
+          },
+        },
+      })
+
+      require('telescope').load_extension('ui-select')
+    end,
+    event = 'VeryLazy',
+  },
+
+  {
     'nvim-telescope/telescope-fzy-native.nvim',
     dependencies = {
       { 'nvim-telescope/telescope.nvim' },
