84958e8eebf5f5ff2d94bb3094871a98875e9bdb
blooper05 <legend.of.blooper@gmail.com>
2025-01-21 19:19:13 +0900

    update

--- config/nvim/lua/plugins/web.lua
+++ config/nvim/lua/plugins/web.lua
@@ -19,27 +19,18 @@ return {
   },
 
   {
-    'robitx/gp.nvim',
+    'David-Kunz/gen.nvim',
     opts = {
-      providers = {
-        openai = { disable = true },
-        ollama = { disable = false },
-      },
-
-      -- stylua: ignore start
-      chat_shortcut_respond = { modes = { 'n', 'i', 'v', 'x' }, shortcut = '<Leader>r' },
-      chat_shortcut_delete  = { modes = { 'n', 'i', 'v', 'x' }, shortcut = '<Leader>d' },
-      chat_shortcut_stop    = { modes = { 'n', 'i', 'v', 'x' }, shortcut = '<Leader>s' },
-      chat_shortcut_new     = { modes = { 'n', 'i', 'v', 'x' }, shortcut = '<Leader>c' },
-      -- stylua: ignore start
+      model = 'gemma2:27b',
+      show_prompt = true,
+      show_model = true,
     },
-    cmd = { 'GpChatNew', 'GpChatPaste', 'GpChatToggle', 'GpChatFinder' },
+    cmd = 'Gen',
     keys = {
       -- stylua: ignore start
-      { '<Leader>g',    function() vim.cmd('GpChatToggle') end,             silent = true },
-      { '<Leader>g',    [[:<C-u>'<,'>GpChatToggle<CR>]],        mode = 'v', silent = true },
-      { '[telescope]G', function() vim.cmd('GpChatFinder') end,             silent = true },
-      -- stylua: ignore start
+      { '<Leader>g', function() vim.cmd('Gen') end,             silent = true },
+      { '<Leader>g', [[:<C-u>'<,'>Gen<CR>]],        mode = 'v', silent = true },
+      -- stylua: ignore end
     },
   },
 
