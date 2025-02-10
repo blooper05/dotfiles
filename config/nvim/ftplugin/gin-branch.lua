7d11742f0d4353353c2b58d10214c77363c7cb26
blooper05 <legend.of.blooper@gmail.com>
2025-02-10 09:59:08 +0900

    update

--- config/nvim/ftplugin/gin-branch.lua
+++
@@ -1,4 +0,0 @@
-vim.opt_local.cursorline = true
-
-vim.keymap.set('n', 'M', '<Plug>(gin-action-move)', { buffer = true })
-vim.keymap.set('n', 'D', '<Plug>(gin-action-delete)', { buffer = true })
--- config/nvim/ftplugin/gin-log.lua
+++
@@ -1,4 +0,0 @@
-vim.opt_local.cursorline = true
-
-vim.keymap.set('n', 'p', '<Plug>(gin-action-show:vsplit)', { buffer = true })
-vim.keymap.set('n', 'if', '<Plug>(gin-action-fixup:instant-fixup)', { buffer = true })
