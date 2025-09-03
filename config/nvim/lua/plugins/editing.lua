a96de6fc1399fb9378917cf70235c75cc4ab2d4d
blooper05 <legend.of.blooper@gmail.com>
2025-09-03 18:41:38 +0900

    update

--- config/nvim/lua/plugins/editing.lua
+++ config/nvim/lua/plugins/editing.lua
@@ -117,10 +117,10 @@ return {
     cmd = { 'DialIncrement', 'DialDecrement' },
     keys = {
       -- stylua: ignore start
-      { '<C-a>',  '<Plug>(dial-increment)',  mode = { 'n', 'v' } },
-      { '<C-x>',  '<Plug>(dial-decrement)',  mode = { 'n', 'v' } },
-      { 'g<C-a>', 'g<Plug>(dial-increment)', mode = { 'n', 'v' } },
-      { 'g<C-x>', 'g<Plug>(dial-decrement)', mode = { 'n', 'v' } },
+      { '<C-a>',  '<Plug>(dial-increment)',   mode = { 'n', 'v' } },
+      { '<C-x>',  '<Plug>(dial-decrement)',   mode = { 'n', 'v' } },
+      { 'g<C-a>', '<Plug>(dial-g-increment)', mode = { 'n', 'v' } },
+      { 'g<C-x>', '<Plug>(dial-g-decrement)', mode = { 'n', 'v' } },
       -- stylua: ignore end
     },
   },
